ef49420ea45ae01d46a2763ca463002f1dfd7499
Indam Muhammad <indam.muhammad@gmail.com>
2015-10-12 03:39:40 +0700

    C45 pruningnya udah tapi belum dicoba huehehe

--- wekaimp/src/wekaimp/MyC45.java
+++ wekaimp/src/wekaimp/MyC45.java
@@ -7,26 +7,39 @@ package wekaimp;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Sourcable;
+import weka.core.AdditionalMeasureProducer;
 import weka.core.Attribute;
 import weka.core.Capabilities;
+import weka.core.Drawable;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Matchable;
 import weka.core.NoSupportForMissingValuesException;
+import weka.core.OptionHandler;
+import weka.core.Statistics;
+import weka.core.Summarizable;
 import weka.core.TechnicalInformation;
 import weka.core.TechnicalInformationHandler;
 import weka.core.Utils;
+import weka.core.WeightedInstancesHandler;
 
 /**
  *
  * @author gifarikautsar
  */
 public class MyC45 extends Classifier 
-    implements TechnicalInformationHandler, Sourcable {
+    implements OptionHandler, Sourcable, 
+             WeightedInstancesHandler, 
+             TechnicalInformationHandler {
     private MyC45[] successors;
     private Attribute attribute;
     private double classValue;
     private double[] distribution;
     private Attribute classAttribute;
+    private boolean isPrune;
+    
+    private Instances m_data;
+    private float confLevel = 0.25f;
     /**
      * @param args the command line arguments
      */
@@ -60,12 +73,14 @@ public class MyC45 extends Classifier
         data = new Instances(data);
         data.deleteWithMissingClass();
 
+        m_data = data;
         makeTree(data);
+        prune();
     }
     
     public double classifyInstance(Instance inst) throws NoSupportForMissingValuesException{
         if(inst.hasMissingValue())
-            throw new NoSupportForMissingValuesException("Id3: no missing values, "
+            throw new NoSupportForMissingValuesException("C45: no missing values, "
                                                    + "please.");
 
         if(attribute == null){
@@ -158,6 +173,116 @@ public class MyC45 extends Classifier
         return splitData;
     }
     
+    public void prune() throws Exception {
+        double errorsLargestBranch;
+        double errorsLeaf;
+        double errorsTree;
+        int indexOfLargestBranch;
+        MyC45 largestBranch;
+        int i;
+
+        if (successors != null){
+
+            // Prune all subtrees.
+            for (i=0;i<attribute.numValues();i++)
+                successors[i].prune();
+
+            // Compute error for largest branch
+            indexOfLargestBranch = indexOfMaxBranch();
+            
+            errorsLargestBranch = successors[indexOfLargestBranch].
+                getEstimatedErrorsForBranch();
+
+            // Compute error if this Tree would be leaf
+            errorsLeaf = getEstimatedErrorsForDistribution(new Distribution(m_data));
+
+            // Compute error for the whole subtree
+            errorsTree = getEstimatedErrors();
+
+            // Decide if leaf is best choice.
+            if (Utils.smOrEq(errorsLeaf,errorsTree+0.1) &&
+                Utils.smOrEq(errorsLeaf,errorsLargestBranch+0.1)){
+
+                // Free son Trees
+                successors = null;
+
+                distribution = new double[m_data.numClasses()];            
+                for(int j=0; j<m_data.numInstances(); j++){
+                    Instance iTemp = (Instance) m_data.instance(j);
+                    distribution[(int) iTemp.classValue()]++;
+                }
+                Utils.normalize(distribution);
+                classValue = Utils.maxIndex(distribution);
+                return;
+            }
+
+            // Decide if largest branch is better choice
+            // than whole subtree.
+            if (Utils.smOrEq(errorsLargestBranch,errorsTree+0.1)){
+                largestBranch = successors[indexOfLargestBranch];
+                successors = largestBranch.successors;
+                prune();
+            }
+        }
+    }
+    
+    private double getEstimatedErrors(){
+        if(successors == null)
+            return 0;
+        else{
+            double errors = 0;
+            for(int i=0; i<successors.length; i++){
+                errors += successors[i].getEstimatedErrors();
+            }
+            return errors;
+        }
+    }
+    
+    private double getEstimatedErrorsForBranch() throws Exception{
+        if(successors == null)
+            return getEstimatedErrorsForDistribution(new Distribution(m_data));
+        else{
+            Distribution dist = new Distribution(m_data);
+            double errors = 0;
+            for(int i=0; i<successors.length; i++){
+                errors += successors[i].getEstimatedErrorsForBranch();
+            }
+            return errors;
+        }
+    }
+    
+    private double getEstimatedErrorsForDistribution(Distribution dist){
+        if (Utils.eq(dist.total(),0))
+            return 0;
+        else
+            return dist.numIncorrect()+
+                addErrs(dist.total(), dist.numIncorrect(),confLevel);
+    }
+    
+    private int indexOfMaxBranch() {
+        int iMax = -1, max = 0;
+        for(int i=0; i<successors.length; i++){
+            int n = successors[i].numOfBranch();
+            if(max < n){
+                max = n; iMax = i;
+            }
+        }
+        
+        return iMax;
+    }
+    
+    private int numOfBranch(){
+        if(successors.length > 0){
+            int num = 1;
+            for(int i=0; i<successors.length; i++){
+                num += successors[i].numOfBranch();
+            }            
+            return num;
+        }
+        else
+            return 1;
+    }
+    
     private String toString(int level){
         StringBuffer printTree = new StringBuffer();
         
@@ -181,6 +306,45 @@ public class MyC45 extends Classifier
         }
         return printTree.toString();
     }
+    
+    public static double addErrs(double N, double e, float CF){
+
+    // Check for extreme cases at the low end because the
+    // normal approximation won't work
+    if (e < 1) {
+
+      // Base case (i.e. e == 0) from documenta Geigy Scientific
+      // Tables, 6th edition, page 185
+      double base = N * (1 - Math.pow(CF, 1 / N)); 
+      if (e == 0) {
+	return base; 
+      }
+    
+      // Use linear interpolation between 0 and 1 like C4.5 does
+      return base + e * (addErrs(N, 1, CF) - base);
+    }
+    
+    // Use linear interpolation at the high end (i.e. between N - 0.5
+    // and N) because of the continuity correction
+    if (e + 0.5 >= N) {
+
+      // Make sure that we never return anything smaller than zero
+      return Math.max(N - e, 0);
+    }
+
+    // Get z-score corresponding to CF
+    double z = Statistics.normalInverse(1 - CF);
+
+    // Compute upper limit of confidence interval
+    double  f = (e + 0.5) / N;
+    double r = (f + (z * z) / (2 * N) +
+		z * Math.sqrt((f / N) - 
+			      (f * f / N) + 
+			      (z * z / (4 * N * N)))) /
+      (1 + (z * z) / N);
+
+    return (r * N) - e;
+  }
     
     @Override
     public TechnicalInformation getTechnicalInformation() {
